726f41b6b3c236c4c8cb89125f15f3adb04c6012
Tim Cane <tim.cane@cloudattribution.com>
2024-06-06 16:18:23 +0000

    added the navigation bar to the home page.

--- src/components/ui/header-navigation.tsx
+++ src/components/ui/header-navigation.tsx
@@ -8,6 +8,7 @@ import { assertIsNode, cn } from "@/lib/utils";
 type HeaderNavigationProps = {
   links: Array<MenuLink | ActionLink>;
   currentPath: string;
+  isTransparent: boolean;
 };
 
 type HeaderNavigationState = {
@@ -34,23 +35,25 @@ class HeaderNavigation extends React.Component<
 
   render() {
     return (
-      <nav className="bg-background">
+      <nav className={cn(this.props.isTransparent ? "relative z-50 text-white" : "bg-background text-foreground",
+        this.props.isTransparent && this.state.showMenu ? "bg-background mb-[72px]" : ""
+      )}>
         <Container className="px-8 py-4">
           <div className="flex flex-wrap items-center justify-between">
             <a
               href="/"
-              className="text-black flex items-center gap-3 hover:no-underline"
+              className={cn(this.props.isTransparent && !this.state.showMenu ? "text-white" : "text-foreground", " flex items-center gap-3 hover:no-underline")}
             >
-              <span className="self-center whitespace-nowrap text-xl font-black text-foreground">
+              <span className={cn(this.props.isTransparent && !this.state.showMenu ? "text-white" : "text-foreground", "self-center whitespace-nowrap text-xl font-black")}>
                 1st Chertsey
               </span>
 
-              <HeaderLogo />
+              <HeaderLogo isTransparent={this.props.isTransparent && !this.state.showMenu} />
             </a>
             <button
               id="toggle"
               type="button"
-              className="inline-flex h-10 w-10 items-center justify-center p-2 text-sm md:hidden"
+              className={cn(this.props.isTransparent && this.state.showMenu ? "text-foreground" : "", "inline-flex h-10 w-10 items-center justify-center p-2 text-sm md:hidden")}
               aria-controls="collapseMenu"
               aria-expanded={this.state.showMenu ? "true" : "false"}
               onClick={this.handleMenuToggle}
@@ -84,12 +87,14 @@ class HeaderNavigation extends React.Component<
                           text={text}
                           links={links}
                           currentPath={this.props.currentPath}
+                          isTransparent={this.props.isTransparent && !this.state.showMenu}
                         />
                       ) : (
                         <Link
                           href={href}
                           text={text}
                           active={href === this.props.currentPath}
+                          isTransparent={this.props.isTransparent && !this.state.showMenu}
                         />
                       )
                     ) : (
@@ -103,7 +108,7 @@ class HeaderNavigation extends React.Component<
                 ))}
               </ul>
               <div className="flex content-center justify-end px-2 pt-4 md:border-l-2 md:pt-0">
-                <ThemeSwitcher />
+                <ThemeSwitcher isTransparent={this.props.isTransparent && !this.state.showMenu} />
               </div>
             </div>
           </div>
@@ -113,7 +118,17 @@ class HeaderNavigation extends React.Component<
   }
 }
 
-class HeaderLogo extends React.Component {
+type HeaderLogoProps = {
+  isTransparent: boolean;
+};
+
+class HeaderLogo extends React.Component<
+  HeaderLogoProps
+> {
+  constructor(props: HeaderLogoProps) {
+    super(props);
+  }
+
   render() {
     return (
       <svg
@@ -122,7 +137,7 @@ class HeaderLogo extends React.Component {
         className="h-8 w-8"
       >
         <path
-          className="fill-foreground"
+          className={cn(this.props.isTransparent ? "fill-white" : "fill-foreground")}
           d="m187.472 358.357-3.815-1.688c-19.634-8.654-38.239-26.282-46.295-43.859l-2.302-5.029 3.226-4.484c7.431-10.283 12.845-22.589 15.235-34.431h19.282c-1.481 9.245-5.409 24.513-15.759 40.612 6.485 10.861 18.126 21.577 30.436 27.986l.019-.011.021.011c12.311-6.409 23.952-17.125 30.436-27.986-10.35-16.099-14.278-31.367-15.759-40.612h19.283c2.39 11.842 7.804 24.148 15.234 34.431l3.227 4.484-2.304 5.029c-8.054 17.577-26.662 35.205-46.293 43.859l-3.817 1.688-.028-.013-.027.013ZM79.093 252.12v-18.193h216.815v18.193H79.093Zm19.691-81.779c-8.098-12.426-21.911-27.239-41.705-27.252h-.096c-11.469 0-21.774 4.964-29.055 13.979-7.596 9.419-10.592 22.1-8.006 33.915l-18.538 4.058c-3.857-17.648.433-35.83 11.779-49.89 10.789-13.376 26.752-21.041 43.814-21.041h.136c22.369.01 42.282 12.413 57.57 35.86 12.854 19.71 18.992 42.002 20.592 57.215h-19.108c-1.643-12.451-6.919-30.806-17.383-46.844Zm38.672-3.553c-8.51-18.894-16.55-36.744-16.426-56.874.219-24.264 13.526-48.593 35.6-65.116 2.603-1.968 16.146-12.289 23.93-20.672l6.939-7.483 6.942 7.483c7.783 8.383 21.327 18.704 23.929 20.672 22.075 16.523 35.381 40.852 35.601 65.116.123 20.13-7.918 37.98-16.426 56.874-7.142 15.857-14.516 32.217-16.879 50.397H201.56c2.383-22.016 11.019-41.201 18.673-58.192 7.644-16.954 14.859-32.972 14.754-48.937-.158-18.355-10.625-37.067-28.003-50.079-3.047-2.292-11.702-8.934-19.485-16.079-7.779 7.145-16.437 13.787-19.481 16.079-17.379 13.012-27.846 31.724-28.005 50.079-.105 15.965 7.11 31.983 14.755 48.937 7.654 16.991 16.29 36.176 18.673 58.192h-19.106c-2.363-18.18-9.737-34.54-16.879-50.397Zm122.862-6.818c15.288-23.447 35.201-35.85 57.57-35.86h.136c17.061 0 33.025 7.665 43.814 21.041 11.344 14.06 15.635 32.242 11.779 49.89l-18.539-4.058c2.586-11.815-.41-24.496-8.006-33.915-7.279-9.015-17.585-13.979-29.054-13.979h-.096c-19.794.013-33.607 14.826-41.705 27.252-10.465 16.038-15.74 34.393-17.383 46.844h-19.108c1.6-15.213 7.738-37.505 20.592-57.215Z"
         />
       </svg>
@@ -152,16 +167,18 @@ type LinkProps = {
   href?: string;
   text?: string;
   active: boolean;
+  isTransparent: boolean;
 };
 class Link extends React.Component<LinkProps> {
   render(): React.ReactNode {
     return (
       <a
         className={cn(
-          "block py-2 px-3 md:p-0 font-semibold text-foreground hover:no-underline hover:text-primary",
+          "block py-2 px-3 md:p-0 font-semibold hover:no-underline",
           {
             "text-primary": this.props.active,
-          }
+          },
+          this.props.isTransparent ? "text-white" : "text-foreground hover:text-primary",
         )}
         href={this.props.href}
       >
@@ -175,6 +192,7 @@ type DropdownLinkProps = {
   text?: string;
   links: Array<MenuLink>;
   currentPath?: string;
+  isTransparent: boolean;
 };
 
 type DropdownLinkState = {
@@ -230,7 +248,7 @@ class DropdownLink extends React.Component<
         <button
           aria-controls={this.id}
           aria-expanded={this.state.show ? "true" : "false"}
-          className="text-neutral-700 block px-3 py-2 font-semibold hover:text-primary hover:no-underline md:p-0"
+          className={cn(this.props.isTransparent ? "text-white" : "text-foreground hover:text-primary", "block px-3 py-2 font-semibold hover:no-underline md:p-0")}
           onClick={this.handleMenuToggle}
         >
           {this.props.text}{" "}
