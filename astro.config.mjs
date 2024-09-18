334b08d7603921b10eb4c9dc4b0ab3ce83ceab97
Tim Cane <3074314+TimCane@users.noreply.github.com.>
2024-09-18 20:03:10 +0000

    added tests around accessibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import pageInsight from "astro-page-insight";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  }), pageInsight()],
+  }), pageInsight(), sitemap()],
   build: {
     assets: "_assets"
   },
